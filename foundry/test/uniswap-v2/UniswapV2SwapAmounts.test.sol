dc2534c923bcc9f3b2796124cf58133158012a57
qfm_win <qifumin2017@163.com>
2026-01-10 14:28:36 +0800

    v2

--- foundry/test/uniswap-v2/UniswapV2SwapAmounts.test.sol
+++ foundry/test/uniswap-v2/UniswapV2SwapAmounts.test.sol
@@ -5,21 +5,23 @@ import {Test, console2} from "forge-std/Test.sol";
 import {IERC20} from "../../src/interfaces/IERC20.sol";
 import {IWETH} from "../../src/interfaces/IWETH.sol";
 import {IUniswapV2Router02} from "../../src/interfaces/uniswap-v2/IUniswapV2Router02.sol";
-import {DAI, WETH, MKR, UNISWAP_V2_ROUTER_02} from "../../src/Constants.sol";
+import {DAI, WETH, MKR, USDC, UNISWAP_V2_ROUTER_02} from "../../src/Constants.sol";
 
 contract UniswapV2SwapAmountsTest is Test {
     IWETH private constant weth = IWETH(WETH);
     IERC20 private constant dai = IERC20(DAI);
     IERC20 private constant mkr = IERC20(MKR);
+    IERC20 private constant usdc = IERC20(USDC);
 
     IUniswapV2Router02 private constant router =
         IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
 
     function test_getAmountsOut() public {
-        address[] memory path = new address[](3);
+        address[] memory path = new address[](4);
         path[0] = WETH;
         path[1] = DAI;
         path[2] = MKR;
+        path[3] = USDC;
 
         uint256 amountIn = 1e18;
         uint256[] memory amounts = router.getAmountsOut(amountIn, path);
@@ -27,19 +29,22 @@ contract UniswapV2SwapAmountsTest is Test {
         console2.log("WETH", amounts[0]);
         console2.log("DAI", amounts[1]);
         console2.log("MKR", amounts[2]);
+        console2.log("USDC", amounts[3]);
     }
 
     function test_getAmountsIn() public {
-        address[] memory path = new address[](3);
+        address[] memory path = new address[](4);
         path[0] = WETH;
         path[1] = DAI;
         path[2] = MKR;
+        path[3] = USDC;
 
-        uint256 amountOut = 1e16;
+        uint256 amountOut = 1e7;
         uint256[] memory amounts = router.getAmountsIn(amountOut, path);
 
         console2.log("WETH", amounts[0]);
         console2.log("DAI", amounts[1]);
         console2.log("MKR", amounts[2]);
+        console2.log("USDC", amounts[3]);
     }
 }
---
+++ foundry/test/uniswap-v2/note.md
@@ -0,0 +1,7 @@
+
+```shell
+FORK_URL=https://eth-mainnet.g.alchemy.com/v2/WyBImvjWu3oYVVD8Fkwimuyrfz8RljJM
+
+admin@Minner-PC MINGW64 ~/Codes/advanced-defi-2024/foundry (main-win-univ2)
+$ forge test --fork-url $FORK_URL --match-path test/uniswap-v2/UniswapV2SwapAmounts.test.sol --mt test_getAmountsIn -vvv
+```
