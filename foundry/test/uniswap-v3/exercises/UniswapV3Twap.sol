15387acfed7bd22e009fe70f7f47433771e8ef1a
qfm_win <qifumin2017@163.com>
2026-03-08 20:23:45 +0800

    twap

--- foundry/test/uniswap-v3/exercises/UniswapV3Twap.sol
+++ foundry/test/uniswap-v3/exercises/UniswapV3Twap.sol
@@ -49,18 +49,21 @@ contract UniswapV3Twap {
         returns (uint256 amountOut)
     {
         // Task 1 - Require tokenIn is token0 or token1
+        require(tokenIn == token0 || tokenIn ==token1, "invalid token");
         // Task 2 - Assign tokenOut
-        address tokenOut;
+        address tokenOut = tokenIn == token0 ? token1 : token0;
 
         // Task 3 - Fill out timeDeltas with dt and 0
         uint32[] memory timeDeltas = new uint32[](2);
-
+        timeDeltas[0] = dt;
+        timeDeltas[1] = 0;
         // Task 4 - Call pool.observe
         // Task 5 - Calculate tickCumulativeDelta
-        int56 tickCumulativeDelta;
+        (int56[] memory tickCumulatives,) = pool.observe(timeDeltas);
+        int56 tickCumulativeDelta = tickCumulatives[1] - tickCumulatives[0];
 
         // Task 6 - Calculate average tick
-        int24 tick;
+        int24 tick = int24(tickCumulativeDelta / int56(uint56(dt)));
 
         // Always round to negative infinity
         if (
@@ -71,5 +74,6 @@ contract UniswapV3Twap {
         }
 
         // Task 7 - Call getQuoteAtTick
+        return getQuoteAtTick(tick, amountIn, tokenIn, tokenOut);
     }
 }
